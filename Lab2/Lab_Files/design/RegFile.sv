41cfabc403b1f8003f37990cd7f3e913f4f8c3e0
snabiee <snabiee@uci.edu>
2019-01-26 20:49:56 -0800

    Lab2

--- Lab2/Lab_Files/design/RegFile.sv
+++ Lab2/Lab_Files/design/RegFile.sv
@@ -17,26 +17,29 @@ module RegFile#(
    input  [DATA_WIDTH-1:0] rg_wrt_data, // data that supposed to be written into the register file
          
    // Outputs
-   output logic [DATA_WIDTH-1:0] rg_rd_data1, //content of reg_file[rg_rd_addr1] is loaded into
-   output logic [DATA_WIDTH-1:0] rg_rd_data2 //content of reg_file[rg_rd_addr2] is loaded into
+   output wire [DATA_WIDTH-1:0] rg_rd_data1, //content of reg_file[rg_rd_addr1] is loaded into
+   output wire [DATA_WIDTH-1:0] rg_rd_data2 //content of reg_file[rg_rd_addr2] is loaded into
    );
 
 
 integer 	 i;
 
-logic [DATA_WIDTH-1:0] register_file [NUM_REGS-1:0];
+reg [DATA_WIDTH-1:0] register_file [NUM_REGS-1:0];
 
-always @(posedge clk) begin
+//always @(posedge clk) begin
 
-	if(rst==1'b1)
-		for (i = 0; i < NUM_REGS; i = i + 1)
-		    register_file[i] <= 0;
-end
+//	if(rst==1'b1)
+//		for (i = 0; i < NUM_REGS; i = i + 1)
+//		    register_file[i] <= 0;
+//end
 
 always @(negedge clk) begin
 
     if(rst==1'b0 && rg_wrt_en==1'b1)
         register_file[rg_wrt_dest] <=rg_wrt_data;
+    else if(rst==1'b1)
+	for (i=0;i<NUM_REGS;i=i+1)
+		register_file[i]<=0;
   
 end
 
