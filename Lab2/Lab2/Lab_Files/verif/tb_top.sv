ca887d37803a6e986360e8b0545c41a7a67d3032
safavis <safavis@uci.edu>
2019-02-11 11:04:23 -0800

    merging

---
+++ Lab2/Lab2/Lab_Files/verif/tb_top.sv
@@ -0,0 +1,31 @@
+`timescale 1ns / 1ps
+
+module tb_top;
+
+//clock and reset signal declaration
+  logic tb_clk, reset;
+  logic [31:0] tb_WB_Data;
+  
+    //clock generation
+  always #10 tb_clk = ~tb_clk;
+  
+  //reset Generation
+  initial begin
+    tb_clk = 0;
+    reset = 1;
+    #25 reset =0;
+  end
+  
+  
+  riscv riscV(
+      .clk(tb_clk),
+      .reset(reset),
+      .WB_Data(tb_WB_Data)      
+     );
+
+  initial begin
+  $readmemb ( "$verif/program/inst.bin" , riscV.dp.instr_mem.Inst_mem);
+    #1000;
+    $finish;
+   end
+endmodule
